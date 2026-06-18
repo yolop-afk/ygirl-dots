752dd2c5135ede79f827acc3bd75744e01539263
yolop-afk <cybert32it@gmail.com>
2026-06-18 18:30:50 -0400

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+fish_add_path /home/yolop/.spicetify
