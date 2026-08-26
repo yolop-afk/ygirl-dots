6c60b8a92e4c919acdbe3b064d1fb8d88db3a367
yolop-afk <cybert32it@gmail.com>
2026-08-25 20:47:10 -0400

    Delete fish directory

--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
-
-fish_add_path /home/yolop/.spicetify
